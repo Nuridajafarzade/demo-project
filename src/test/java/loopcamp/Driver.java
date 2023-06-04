4f53f31129fbeeada9154d6fca314322ae1b9de3
nuridajafarzade <nuridacaferi@gmail.com>
2023-06-03 22:46:26 -0400

    ip address update

--- src/test/java/loopcamp/Driver.java
+++ src/test/java/loopcamp/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "35.175.139.60";
+                        String gridAddress = "18.206.172.132";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
@@ -47,7 +47,7 @@ public class Driver {
                 case "remote-firefox":
                     try {
                         // assign your grid server address
-                        String gridAddress = "35.175.139.60";
+                        String gridAddress = "18.206.172.132";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
